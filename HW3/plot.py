12da43d85fc8acbb073b779fc56b8db601cfc312
Eric Mascot <slek120@gmail.com>
2016-04-20 13:44:33 -0500

    clean

--- HW3/plot.py
+++ HW3/plot.py
@@ -1,4 +1,4 @@
-from scipy import *
+from scipy import loadtxt, linspace
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
@@ -8,13 +8,13 @@ def E(x, k, a, E0):
 dat = loadtxt('total_E.dat')
 x   = dat[:,0]
 y   = dat[:,1]
-k   = 5.
+k0  = 5.
 a0  = 2.46
 E0  = -22.79119210
 
-popt, pcov = curve_fit(E, x, y, p0=(k, a0, E0))
+popt, pcov = curve_fit(E, x, y, p0=(k0, a0, E0))
 
-x0  = linspace(x[0],x[-1],20)
+x0  = linspace(x[0],x[-1],50)
 y0  = E(x0, *popt)
 lab = '$k=%.4f$\n$a_0=%.4f$\n$E_0=%.4f$'%(popt[0], popt[1], popt[2])
 
@@ -23,6 +23,7 @@ plt.plot(x0,y0,label=lab)
 plt.xlabel('$a$')
 plt.ylabel('$E$')
 plt.legend(loc='best')
+plt.savefig('total_E.eps')
 plt.savefig('total_E.png')
 
 print(popt)
